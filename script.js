09d2ce4eeaff25346b28c54762ef6c5fff7e2302
Cart-Monke <jacob.velasquez@student.cart.org>
2023-03-03 08:14:44 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const cowtainer = document.getElementById("cowtainer");
+const cownter = document.getElementById("cownter");
+
+let cownt = 0;
+let root = document.querySelector(':root');
+
+function make_cow() {
+    let new_cow = document.createElement("img");
+    new_cow.src = "cow.jpg";
+    cownt++;
+    cownter.innerHTML = `Cow count: ${cownt}`
+    cowtainer.appendChild(new_cow);
+    
+    let sqr = Math.sqrt(cownt);
+
+    if (sqr == Math.floor(sqr)) {
+        root.style.setProperty('--grid-size', sqr + 1);
+    }
+
+    setTimeout(() => {
+        make_cow();
+    }, 10);
+}
+
+make_cow();
+
+document.querySelector("body").addEventListener("mouseleave", _ => {
+    alert("DONT LEAVE MEE");
+})
