fe78dbda9c411843804398e76fad519e9f54cdf7
Cart-Monke <jacob.velasquez@student.cart.org>
2023-03-30 07:57:47 -0700

    Updated to be more annoying

--- script.js
+++ script.js
@@ -2,28 +2,29 @@ const cowtainer = document.getElementById("cowtainer");
 const cownter = document.getElementById("cownter");
 
 let cownt = 0;
-let root = document.querySelector(':root');
+let root = document.querySelector(":root");
 
 function make_cow() {
-    let new_cow = document.createElement("img");
-    new_cow.src = "cow.jpg";
-    cownt++;
-    cownter.innerHTML = `Cow count: ${cownt}`
-    cowtainer.appendChild(new_cow);
-    
-    let sqr = Math.sqrt(cownt);
+  let new_cow = document.createElement("img");
+  new_cow.src = "cow.jpg";
+  cownt++;
+  cownter.innerHTML = `Cow count: ${cownt}`;
+  cowtainer.appendChild(new_cow);
 
-    if (sqr == Math.floor(sqr)) {
-        root.style.setProperty('--grid-size', sqr + 1);
-    }
+  let sqr = Math.sqrt(cownt);
 
-    setTimeout(() => {
-        make_cow();
-    }, 10);
+  if (sqr == Math.floor(sqr)) {
+    root.style.setProperty("--grid-size", sqr + 1);
+  }
+
+  setTimeout(() => {
+    make_cow();
+  }, 10);
 }
 
 make_cow();
 
-document.querySelector("body").addEventListener("mouseleave", _ => {
-    alert("DONT LEAVE MEE");
-})
+document.querySelector("body").addEventListener("mouseleave", (_) => {
+  alert("DONT LEAVE MEE");
+  window.open("https://broimagine.netlify.app/", "_blank").focus();
+});
